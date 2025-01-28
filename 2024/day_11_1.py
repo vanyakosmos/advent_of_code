60c1ecea7c6efdca02c323c3d87d755b9a25b77c
Ivan Bachynin <bachynin.i@gmail.com>
2025-01-29 00:20:26 +0200

    add 2024/11

---
+++ 2024/day_11_1.py
@@ -0,0 +1,25 @@
+from utils import print_result, read_data
+
+
+def main():
+    line = read_data(1)[0]
+    nums = [int(n) for n in line.split()]
+
+    for i in range(25):
+        new_nums = []
+        for n in nums:
+            if n == 0:
+                new_nums.append(1)
+            elif len(str(n)) % 2 == 0:
+                sn = str(n)
+                new_nums.append(int(sn[: len(sn) // 2]))
+                new_nums.append(int(sn[len(sn) // 2 :]))
+            else:
+                new_nums.append(n * 2024)
+        nums = new_nums
+
+    print_result(len(nums))
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2024/day_11_2.py
@@ -0,0 +1,43 @@
+from collections import defaultdict
+from functools import lru_cache
+
+from utils import print_result, read_data
+
+
+def algo(n: int) -> list[int]:
+    if n == 0:
+        return [1]
+    elif len(str(n)) % 2 == 0:
+        sn = str(n)
+        return [
+            int(sn[: len(sn) // 2]),
+            int(sn[len(sn) // 2 :]),
+        ]
+    else:
+        return [n * 2024]
+
+
+@lru_cache(maxsize=None)
+def run(n, it) -> int:
+    if it == 0:
+        return 1
+    counter = defaultdict(int)
+    for next_num in algo(n):
+        counter[next_num] += 1
+    su = 0
+    for num, count in counter.items():
+        su += run(num, it - 1) * count
+    return su
+
+
+def main():
+    line = read_data(1)[0]
+    nums = [int(n) for n in line.split()]
+    res = 0
+    for n in nums:
+        res += run(n, 75)
+    print_result(res)
+
+
+if __name__ == "__main__":
+    main()
